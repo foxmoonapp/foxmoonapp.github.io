1c2c7486daa9798aa41cdcd9f2ab749e97ff5b86
alireza bonab <alireza.bonab@ipsos.com>
2021-01-27 14:56:32 +0100

    - make playground interactive - parser and transfomer box are hidden by default - detailview checkbox added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+let yamlInput;
+let parserInput;
+let transformerInput;
+let codeGeneratorInput;
+let chkDetailView;
+let parserCodeBox;
+let transfomerCodeBox;
+
 const init = async () => {
   const initialCode = `kind: component
 name: hello
@@ -12,23 +20,51 @@ spec:
       - type: prop
         value: name`;
 
-  const yamlInput = document.querySelector('#yaml');
-  const parserInput = document.querySelector('#parser');
-  const transformerInput = document.querySelector('#transformer');
-  const codeGeneratorInput = document.querySelector('#codeGenerator');
+  yamlInput = document.querySelector('#yaml');
+  parserInput = document.querySelector('#parser');
+  transformerInput = document.querySelector('#transformer');
+  codeGeneratorInput = document.querySelector('#codeGenerator');
+  chkDetailView = document.querySelector('#chkDetailView');
+  parserCodeBox = document.querySelector('#parserCodeBox');
+  transfomerCodeBox = document.querySelector('#transfomerCodeBox');
 
-  yamlInput.textContent = initialCode;
+  parserCodeBox.style.display = 'none';
+  transfomerCodeBox.style.display = 'none';
+  chkDetailView.addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      parserCodeBox.style.display = 'flex';
+      transfomerCodeBox.style.display = 'flex';
+    } else {
+      parserCodeBox.style.display = 'none';
+      transfomerCodeBox.style.display = 'none';
+    }
+  });
 
-  const lexerComponent = await compiler.Lexer.yamlLexer(yaml.textContent);
-  const parsedComponent = await compiler.Parser.parser(lexerComponent);
-  parserInput.textContent = JSON.stringify(parsedComponent, null, ' ');
-
-  const transfomedComponent = await compiler.Transformer.transform(parsedComponent);
-  transformerInput.textContent = JSON.stringify(transfomedComponent, null, ' ');
-  
-  const generatedCode = await compiler.CodeGenerator.codeGenerator(transfomedComponent);
-  codeGeneratorInput.textContent = generatedCode;
+  yamlInput.addEventListener("input", (event) => lexerInputTextChange(event));
+  yamlInput.value = initialCode;
+  update(initialCode);
 };
+
+function lexerInputTextChange(event) {
+  update(event.currentTarget.value);
+}
+
+async function update(yaml) {
+  try {
+    const lexerComponent = await compiler.Lexer.yamlLexer(yaml);
+    const parsedComponent = await compiler.Parser.parser(lexerComponent);
+    parserInput.textContent = JSON.stringify(parsedComponent, null, ' ');
+
+    const transfomedComponent = await compiler.Transformer.transform(parsedComponent);
+    transformerInput.textContent = JSON.stringify(transfomedComponent, null, ' ');
+
+    const generatedCode = await compiler.CodeGenerator.codeGenerator(transfomedComponent);
+    codeGeneratorInput.textContent = generatedCode;
+  } catch (e) {
+    const errStr = `- ${JSON.stringify(e)} `;
+    codeGeneratorInput.textContent = `invalid input ${errStr === '- {} ' ? '' : errStr}...`;
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   init();
